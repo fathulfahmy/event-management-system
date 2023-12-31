d80ec086141dd388f1528521c685fd7ea5151a15
wanhaziqiskandar <wanhaziqiskandar@gmail.com>
2024-01-01 01:15:11 +0800

    feat: addevent php, insertevent php, and manageevent php

---
+++ php/addevent.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Add Event</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="../css/colormodes.css" />
+    <link rel="stylesheet" href="../css/navbar.css" />
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <main class="col-md-9 col-lg-10 px-md-4">
+                <h2 class="my-3 pb-3">Add Event</h2>
+
+                <!-- Form to submit event details to insertevent.php -->
+                <form method="POST" action="insertevent.php" >
+                    <div class="mb-3">
+                        <label for="eventName" class="form-label">Event Name</label>
+                        <input type="text" class="form-control" id="eventName" name="eventName" required>
+                    </div>
+                    <!--<div class="mb-3">
+                        <label for="participants" class="form-label">Participants</label>
+                        <input type="number" class="form-control" id="participants" name="participants" required>
+                    </div>-->
+                    <div class="mb-3">
+                        <label for="quota" class="form-label">Quota</label>
+                        <input type="number" class="form-control" id="quota" name="quota" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </main>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+    <script src="../js/colormodes.js"></script>
+    <script src="../js/navbar.js"></script>
+</body>
+
+</html>
