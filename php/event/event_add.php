b4004b5d75bebeedde8a5a35d9c56e2c11065f49
fathulfahmy <fathulfmy@gmail.com>
2024-01-05 09:22:17 +0800

    feat: remove unused css and js

--- php/event/event_add.php
+++ php/event/event_add.php
@@ -8,7 +8,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="../../css/colormodes.css" />
   <link rel="stylesheet" href="../../css/navbar.css" />
-  <link rel="stylesheet" href="../../assets/bootstrap/examplename/example.css" />
 </head>
 
 <body>
@@ -89,8 +88,8 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="../../js/colormodes.js"></script>
+  <script src="../../js/navbar.js"></script>
   <script src="../../js/validateform.js"></script>
-  <script src="../../assets/bootstrap/example/example.js"></script>
 </body>
 
 </html>
--- php/participant/participant_search.php
+++ php/participant/participant_search.php
@@ -8,13 +8,10 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- 1. document title -->
   <title>Manage Participants</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <link rel="stylesheet" href="../css/colormodes.css" />
-  <link rel="stylesheet" href="../css/navbar.css" />
-  <!-- 2. bootstrap example css -->
-  <link rel="stylesheet" href="../assets/bootstrap/examplename/example.css" />
+  <link rel="stylesheet" href="../../css/colormodes.css" />
+  <link rel="stylesheet" href="../../css/navbar.css" />
 </head>
 
 <body>
@@ -116,13 +113,10 @@
 
   <?php endif; ?>
 
-  <!-- 4. main content end -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  <script src="../js/colormodes.js"></script>
-  <script src="../js/navbar.js"></script>
-  <script src="../js/validateform.js"></script>
-  <!-- 3. bootstrap example js -->
-  <script src="../assets/bootstrap/examplename/example.js"></script>
+  <script src="../../js/colormodes.js"></script>
+  <script src="../../js/navbar.js"></script>
+  <script src="../../js/validateform.js"></script>
 </body>
 
 </html>
