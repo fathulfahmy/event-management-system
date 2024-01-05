65f2f6d0f671267bf8314e653796cceeffb97f9f
fathulfahmy <fathulfmy@gmail.com>
2024-01-06 00:26:10 +0800

    feat: update database

--- assets/phpmyadmin/esportsdb.sql
+++ assets/phpmyadmin/esportsdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 05, 2024 at 04:07 PM
+-- Generation Time: Jan 05, 2024 at 05:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `esportsdb`
 --
+CREATE DATABASE IF NOT EXISTS `esportsdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `esportsdb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE `admins` (
+  `adminid` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL DEFAULT 'unitenesports',
+  `email` varchar(30) NOT NULL DEFAULT 'esports@uniten.edu.my',
+  `password` varchar(30) NOT NULL DEFAULT 'admin123'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`adminid`, `firstname`, `lastname`, `username`, `email`, `password`) VALUES
+(0001, 'Muhammad Fathul Fahmy', 'Bin Mohd Nizam', 'unitenesports', 'esports@uniten.edu.my', 'admin');
 
 -- --------------------------------------------------------
 
@@ -28,6 +53,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `events`
 --
 
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
   `eventid` int(4) UNSIGNED ZEROFILL NOT NULL,
   `eventname` varchar(30) NOT NULL,
@@ -41,11 +67,68 @@ CREATE TABLE `events` (
 INSERT INTO `events` (`eventid`, `eventname`, `quota`) VALUES
 (0001, 'Valorant', 3),
 (0002, 'Counter-Strike 2', 3),
-(0003, 'EA Sports FC 24', 2);
+(0003, 'EA Sports FC 24', 2),
+(0004, 'The Finals', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participants`
+--
+
+DROP TABLE IF EXISTS `participants`;
+CREATE TABLE `participants` (
+  `participantid` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `participants`
+--
+
+INSERT INTO `participants` (`participantid`, `firstname`, `lastname`, `username`, `email`, `password`) VALUES
+(0001, 'Akmal Izudin', 'Bin Zakaria', 'leaxdomo', 'akmalizudin@gmail.com', 'akmal123'),
+(0002, 'Muhammad Safwan', 'Bin Serat', 'iwansafwan', 'safwanserat@gmail.com', 'safwan123'),
+(0003, 'Mus\'ab Salihin', 'Bin Mustaffa', 'musabsalihin', 'musabsalihin@gmail.com', 'musab123'),
+(0004, 'Wan Haziq Iskandar', 'Bin Wan Izhan', 'haziqiskandar', 'haziqiskandar@gmail.com', 'haziq123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrations`
+--
+
+DROP TABLE IF EXISTS `registrations`;
+CREATE TABLE `registrations` (
+  `registrationid` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `participantid` int(4) UNSIGNED ZEROFILL NOT NULL,
+  `eventid` int(4) UNSIGNED ZEROFILL NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registrations`
+--
+
+INSERT INTO `registrations` (`registrationid`, `participantid`, `eventid`) VALUES
+(0001, 0001, 0001),
+(0002, 0002, 0002),
+(0003, 0003, 0001),
+(0013, 0002, 0003),
+(0014, 0004, 0003);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`adminid`);
 
 --
 -- Indexes for table `events`
@@ -54,14 +137,57 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`eventid`);
 
 --
+-- Indexes for table `participants`
+--
+ALTER TABLE `participants`
+  ADD PRIMARY KEY (`participantid`);
+
+--
+-- Indexes for table `registrations`
+--
+ALTER TABLE `registrations`
+  ADD PRIMARY KEY (`registrationid`),
+  ADD KEY `participantid` (`participantid`),
+  ADD KEY `eventid` (`eventid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `adminid` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `eventid` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `eventid` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `participants`
+--
+ALTER TABLE `participants`
+  MODIFY `participantid` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `registrations`
+--
+ALTER TABLE `registrations`
+  MODIFY `registrationid` int(4) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `registrations`
+--
+ALTER TABLE `registrations`
+  ADD CONSTRAINT `registrations_ibfk_1` FOREIGN KEY (`participantid`) REFERENCES `participants` (`participantid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `registrations_ibfk_2` FOREIGN KEY (`eventid`) REFERENCES `events` (`eventid`) ON DELETE CASCADE ON UPDATE CASCADE;
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
